b349ce240bbd722e10c1b50553db8c0698ea4690
Maria Romero <m.romeroangulo@gmail.com>
2017-04-05 12:41:15 +0200

    cambio a os.env

--- eventful.py
+++ eventful.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import requests
 from lxml import etree
+import os
+
+url_base = 'http://api.eventful.com/rest'
+key = os.environ["KEY"]
 
 ciudad = raw_input("Introduce una ciudad: ")
 tipo = raw_input("Introduce un tipo de evento: ")
-key = "JQcrv9s2TtsfKHLG"
+
 payload = {'app_key':key, 'keywords': tipo, 'location':ciudad, 'date':'Future'}
 
-r= requests.get('http://api.eventful.com/rest/events/search', params=payload)
+r= requests.get(url_base+'/events/search', params=payload)
 
 if r.status_code == 200:
 	doc = etree.fromstring(r.text.encode('utf-8'))
